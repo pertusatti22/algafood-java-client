0a1de6ecf061967d8a52a363d129b6bfb968e09c
pertusatti22 <pertusatti22@gmail.com>
2023-07-16 22:21:48 -0300

    Initial Commit

---
+++ src/main/java/com/algaworks/algafoodjavaclient/AlgafoodJavaClientApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafoodjavaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgafoodJavaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgafoodJavaClientApplication.class, args);
+    }
+
+}
